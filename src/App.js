bd17136248e3bd42213bd1261cfcd834d6a4d78f
toks_philip@yahoo.com <toks_philip@yahoo.com>
2019-12-18 07:59:21 +0000

    chg label

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Course Manager
+          Learn React Course Manager Flux
         </a>
       </header>
     </div>
